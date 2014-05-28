3cef733aac82683566acb486db8c7aeca4cabf8f
Andrey Sokolov <a.sokolov@speople.pro>
2014-05-28 14:26:36 +0700

    remove environment from full app name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,11 +9,12 @@ set :server_name, "www.example.com example.com"
 # used in case we're deploying multiple versions of the same
 # app side by side. Also provides quick sanity checks when looking
 # at filepaths
-set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
+# set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
+set :full_app_name, "#{fetch(:application)}"
 
 server 'example.com', user: 'deploy', roles: %w{web app db}, primary: true
 
-set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/#{fetch(:full_app_name)}"
+set :deploy_to, "/home/#{fetch(:deploy_user)}/#{fetch(:full_app_name)}"
 
 # dont try and infer something as important as environment from
 # stage name.
